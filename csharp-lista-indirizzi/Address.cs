680ff625a1567642538b5c1cb1b4ac038c07187b
Hirisci <www.alan92@icloud.com>
2022-09-23 15:03:28 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,32 @@
+﻿//Oggi esercitazione veloce, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+//In questo esercizio dovrete leggere un file CSV,
+//che cambia poco da quanto appena visto nel live-coding in classe,
+//e salvare tutti gli indirizzi contenuti al sul interno all’interno
+//di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+
+internal class Address
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string PostalCode { get; set; }
+
+    public Address(string name, string surname, string street, string city, string province, string postalCode)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        PostalCode = postalCode;
+    }
+
+    public override string? ToString()
+    {
+        return String.Format("| Nome: {0,15} | Cognome: {1,15} | Via: {2,40} | Citta: {3,15}({4,2}) | Zip: {5}", Name,Surname,Street,City,Province,PostalCode);
+
+
+    }
+}
---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,55 @@
+﻿//Oggi esercitazione veloce, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+//In questo esercizio dovrete leggere un file CSV,
+//che cambia poco da quanto appena visto nel live-coding in classe,
+//e salvare tutti gli indirizzi contenuti al sul interno all’interno
+//di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+
+//Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+
+
+using System.IO;
+List<Address> lists = new();
+string path =
+    @"..\..\..\addresses.csv";
+
+
+try
+{
+	using (StreamReader sr = new StreamReader(path))
+	{
+		sr.ReadLine();
+		while (sr.Peek() >= 0)
+		{
+			string[] address = sr.ReadLine().Split(',');
+			try
+			{
+				if (address.Length <=6)
+				{
+					lists.Add(new Address(address[0].Trim(), address[1].Trim(), address[2].Trim(), address[3].Trim(), address[4].Trim(), address[5].Trim()));
+				}
+			}
+            catch (IndexOutOfRangeException e)
+            {
+				sr.ReadLine();
+                
+            }
+        }
+	}
+}
+catch (FileNotFoundException e)
+{
+
+	Console.WriteLine(e.Message);
+}
+catch (Exception)
+{
+
+    throw;
+}
+
+
+
+foreach (Address address in lists)
+{
+	Console.WriteLine(address.ToString());
+}
